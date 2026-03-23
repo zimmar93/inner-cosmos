268d6b1ed9402e1781d8b6a2e9f019f99cc82566
zimmar93 <zimmar.electronic@gmail.com>
2026-03-23 22:30:40 +0200

    Remove Google Fonts links from RootLayout
    Removed Google Fonts preconnect and stylesheet links from the layout.

--- store-frontend/app/layout.tsx
+++ store-frontend/app/layout.tsx
@@ -11,11 +11,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,300&display=swap" rel="stylesheet" />
-            </head>
             <body>
                 <ClientLayout>{children}</ClientLayout>
                 <Toaster
